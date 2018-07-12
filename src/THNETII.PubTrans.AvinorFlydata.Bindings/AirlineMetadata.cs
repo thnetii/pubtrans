0878b4126b6843216a6ffb441d219fde7442b8e7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-07-12 17:57:25 +0200

    Add Avinor Flydata Airline Metadata

---
+++ src/THNETII.PubTrans.AvinorFlydata.Bindings/AirlineMetadata.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace THNETII.PubTrans.AvinorFlydata.Bindings
+{
+    [XmlType, DebuggerDisplay("{" + nameof(DebuggerDisplay) + "()}")]
+    public class AirlineMetadata
+    {
+        [XmlAttribute("code")]
+        public string IataCode { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        private string DebuggerDisplay() => $"{GetType()}(IATA: {IataCode}, {Name})";
+    }
+
+    [XmlRoot("airlineNames")]
+    [XmlType, DebuggerDisplay("{" + nameof(DebuggerDisplay) + "()}")]
+    public class AirlineMetadataListing
+    {
+        public static XmlSerializer DefaultSerializer
+        { get => new XmlSerializer(typeof(AirlineMetadataListing)); }
+
+        [DebuggerStepThrough]
+        public AirlineMetadataListing() { }
+
+        public AirlineMetadataListing(AirlineMetadata[] airlines) : this()
+        {
+            AirlineNames = airlines ?? Array.Empty<AirlineMetadata>();
+        }
+
+        [XmlElement("airlineName")]
+        [SuppressMessage(category: null, "CA1819", Justification = "Must be array for XML serialization.")]
+        public AirlineMetadata[] AirlineNames { get; set; }
+
+        private string DebuggerDisplay() => $"{GetType()}({nameof(AirlineNames.Length)}: {AirlineNames?.Length})";
+    }
+}
---
+++ test/THNETII.PubTrans.Test/AvinorFlydata/Bindings/Test/AirlineMetadataTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Linq;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace THNETII.PubTrans.AvinorFlydata.Bindings.Test
+{
+    public class AirlineMetadataTest
+    {
+        private readonly IFileInfo datafile = Datafiles.GetAirlineNames();
+
+        [Fact]
+        public void CanDeserializeXml()
+        {
+            var serializer = AirlineMetadataListing.DefaultSerializer;
+            AirlineMetadataListing datalisting;
+            using (var dataStream = datafile.CreateReadStream())
+            {
+                datalisting =
+                    (AirlineMetadataListing)serializer.Deserialize(dataStream);
+            }
+            Assert.NotNull(datalisting);
+            Assert.NotNull(datalisting.AirlineNames);
+            Assert.DoesNotContain(datalisting.AirlineNames, airline => string.IsNullOrWhiteSpace(airline?.IataCode));
+        }
+    }
+}
