6796743f5f50f8b583a1ef90bf8a1338c7a4db1c
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-07-13 01:12:17 +0200

    Add Avinor Flydata flight status text metadata classes

---
+++ src/THNETII.PubTrans.AvinorFlydata.Bindings/FlightStatusCode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Serialization;
+using THNETII.Common;
+using THNETII.Common.XmlSerializer;
+
+namespace THNETII.PubTrans.AvinorFlydata.Bindings
+{
+    [XmlType(TypeName = "statusCodeType")]
+    public enum FlightStatusCode
+    {
+        [XmlEnum("")]
+        Unspecified = 0,
+        [XmlEnum("N")]
+        NewInfo,
+        [XmlEnum("E")]
+        EstimatedTime,
+        [XmlEnum("A")]
+        Landed,
+        [XmlEnum("C")]
+        Cancelled,
+        [XmlEnum("D")]
+        Departed,
+        [XmlEnum("O")]
+        Arrived,
+        Unknown = -1
+    }
+
+    public static class FlightStatusCodeHelpers
+    {
+        public static DuplexConversionTuple<string, FlightStatusCode> GetDuplexConversionTuple()
+        {
+            return new DuplexConversionTuple<string, FlightStatusCode>(
+                GetCodeFromString, StringComparer.OrdinalIgnoreCase,
+                XmlEnumStringConverter<FlightStatusCode>.ToString 
+                );
+        }
+
+        private static FlightStatusCode GetCodeFromString(string s)
+        {
+            FlightStatusCode GetUnknownOrUnspecified(string u)
+            {
+                return string.IsNullOrWhiteSpace(u)
+                    ? FlightStatusCode.Unspecified
+                    : FlightStatusCode.Unknown
+                    ;
+            }
+            return XmlEnumStringConverter<FlightStatusCode>.ParseOrDefault(s,
+                GetUnknownOrUnspecified
+                );
+        }
+    }
+}
---
+++ src/THNETII.PubTrans.AvinorFlydata.Bindings/FlightStatusText.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Xml.Serialization;
+using THNETII.Common;
+
+namespace THNETII.PubTrans.AvinorFlydata.Bindings
+{
+    [XmlType, DebuggerDisplay("{" + nameof(DebuggerDisplay) + "()}")]
+    public class FlightStatusText
+    {
+        private readonly DuplexConversionTuple<string, FlightStatusCode> code =
+            FlightStatusCodeHelpers.GetDuplexConversionTuple();
+
+        [XmlAttribute("code", DataType = "NMTOKEN")]
+        public string CodeString {
+            get => code.RawValue;
+            set => code.RawValue = value;
+        }
+
+        [XmlIgnore]
+        public FlightStatusCode Code
+        {
+            get => code.ConvertedValue;
+            set => code.ConvertedValue = value;
+        }
+
+        [XmlAttribute("statusTextEn")]
+        public string TextEnglish { get; set; }
+
+        [XmlAttribute("statusTextNo")]
+        public string TextNorwegian { get; set; }
+
+        private string DebuggerDisplay() => $"{nameof(FlightStatusText)}({nameof(Code)}: {CodeString} ({Code}), {TextNorwegian})";
+    }
+}
