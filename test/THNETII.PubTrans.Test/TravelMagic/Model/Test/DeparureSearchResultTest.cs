5866f28e2be82a700910544766251927e6e94427
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-08-22 15:56:14 +0200

    Add model public property access testing

--- test/THNETII.PubTrans.Test/TravelMagic/Model/Test/DeparureSearchResultTest.cs
+++ test/THNETII.PubTrans.Test/TravelMagic/Model/Test/DeparureSearchResultTest.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Serialization;
+
 using Microsoft.Extensions.FileProviders;
 
 using Xunit;
@@ -22,18 +25,52 @@ namespace THNETII.PubTrans.TravelMagic.Model.Test
                 var result = (DepartureSearchResult)serializer.Deserialize(xmlReader);
 
                 Assert.NotNull(result);
-#if DEBUG
-                Assert.All(result.Departures, dep =>
+            }
+        }
+
+        [Fact]
+        public void CanAccessAllPublicInstancePropertiesWithGetters()
+        {
+            var serializer = new XmlSerializer(typeof(DepartureSearchResult));
+
+            using (var dataStream = xmlfile.CreateReadStream())
+            using (var xmlReader = XmlReader.Create(dataStream))
+            {
+                var result = (DepartureSearchResult)serializer.Deserialize(xmlReader);
+
+                Assert.NotNull(result);
+                AssertItemProperties(result);
+            }
+
+            void AssertPublicGetProperties(object instance)
+            {
+                var props = instance.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                    .Where(pi => pi.CanRead);
+                Assert.All(props, pi =>
                 {
-                    Assert.Empty(dep.UnmatchedAttributes ?? Array.Empty<XmlAttribute>());
-                    Assert.Empty(dep.UnmatchedElements ?? Array.Empty<XmlElement>());
+                    var pv = pi.GetValue(instance);
+                    AssertItemProperties(pv);
                 });
-                Assert.All(result.Stages, st =>
+            }
+
+            void AssertItemProperties(object item)
+            {
+                if (item is null)
+                    return;
+                if (item is IEnumerable<object> items && !(item is string))
                 {
-                    Assert.Empty(st.UnmatchedAttributes ?? Array.Empty<XmlAttribute>());
-                    Assert.Empty(st.UnmatchedElements ?? Array.Empty<XmlElement>());
+                    AssertEnumerableItemProperties(items);
+                }
+                else if (item.GetType().Assembly == typeof(TravelMagicUtils).Assembly)
+                    AssertPublicGetProperties(item);
+            }
+
+            void AssertEnumerableItemProperties(IEnumerable<object> enumerable)
+            {
+                Assert.All(enumerable ?? Enumerable.Empty<object>(), item =>
+                {
+                    AssertItemProperties(item);
                 });
-#endif
             }
         }
     }
